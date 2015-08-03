3623a237ddb545e8f268ab01493c72c336403c7c
陈丰 <gerpayt@qq.com>
2015-08-03 12:22:56 +0800

    兼容IE低版本浏览器

--- assets/js/datetimecompare.validation.js
+++ assets/js/datetimecompare.validation.js
@@ -21,8 +21,15 @@ $.extend(yii.validation, (function ($) {
                 compareValue = $('#' + options.compareAttribute).val();
             }
 
-            var dateValue = (new Date(value)).getTime();
-            var dateCompareValue = (new Date(compareValue)).getTime() + options.offset * 1000;
+            var dateAndTime1 = value.split(' ');
+            var dateValue = (new Date(dateAndTime1[0])).getTime();
+            var hourMinuteSecond1 = dateAndTime1[1].split(':');
+            dateValue = dateValue + (hourMinuteSecond1[0] * 3600 + hourMinuteSecond1[1] * 60 + hourMinuteSecond1[2]) * 1000;
+
+            var dateAndTime2 = compareValue.split(' ');
+            var dateCompareValue = (new Date(dateAndTime2[0])).getTime() + options.offset * 1000;
+            var hourMinuteSecond2 = dateAndTime2[1].split(':');
+            dateCompareValue = dateCompareValue + (hourMinuteSecond2[0] * 3600 + hourMinuteSecond2[1] * 60 + hourMinuteSecond2[2]) * 1000;
             // TODO offset to ===
 
             switch (options.operator) {
